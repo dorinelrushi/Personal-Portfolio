4e2a1b2b705e6010f20c5ab2c68413ab110278b2
dorinelrushi <rushidorinel@gmail.com>
2023-07-01 22:03:54 +0200

    first commit

--- server.js
+++ server.js
@@ -1,17 +1,16 @@
-const express = require('express');
+const express = require("express");
 const PORT = process.env.PORT || 6969;
-
 
 const app = express();
 
-app.set("view engine",'ejs')
+app.set("view engine", "ejs");
 
-app.use('/public',express.static('public'));
+app.use("/public", express.static("public"));
 
+app.get("/", (req, res) => {
+  res.render("index");
+});
 
-app.get('/',(req,res)=>{
-    res.render('index')
-})
-
-
-app.listen(PORT,()=>{console.log(`port is running on port ${PORT}`)})
+app.listen(PORT, () => {
+  console.log(`port is running on port ${PORT}`);
+});
